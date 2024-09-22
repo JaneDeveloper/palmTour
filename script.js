c48724790ed4f5a1087540c20ef5d9f44b793813
Evgeniia  Chidester <evgeniiachidester@Evgeniias-MBP.localdomain>
2024-09-22 13:28:33 -0400

    Added timer, back buttons, correct name setting.

--- script.js
+++ script.js
@@ -8,14 +8,22 @@
         window.location.href='introduction.html';
         }
     }
-    window.onload=function() {
+
+    $(document).ready(()=> {
         var name=localStorage.getItem('userName');
         document.getElementById('welcomeMessage').innerText = 'Hello, ' + name + '!';
-       
-    }
+    })
+
     function startGame (){
+        let startTime = new Date();
+        localStorage.setItem('currentTime', startTime); 
         window.location.href="questionOne.html";
+
     }
+    $("#prev").click(() => location = "index.html");
+   
+
+
     function checkForm() {
         var answer=document.getElementById('answerInput').value;
         if (answer==""){
@@ -26,10 +34,12 @@
             document.getElementById("errorMessage").innerText="Your answer is wrong. Try Again.";
         }
     }
-
+    
     document.addEventListener("DOMContentLoaded", ()=>{
         document.querySelector("#next").addEventListener("click", checkForm);
+        $("#prevOne").click(() => location.replace ("introduction.html"));
     });
+ 
 
     function goToQuestionTwo() {
         window.location.href="questionTwo.html";
@@ -37,6 +47,7 @@
 
     document.addEventListener("DOMContentLoaded", ()=>{
         document.querySelector("#nextQuestion").addEventListener("click", goToQuestionTwo);
+        $("#prevTwo").click(() => location.replace("questionOne.html"));
     });
 
 
@@ -46,23 +57,34 @@
         var selectedOption = document.querySelector('input[name="answer"]:checked');
         if (selectedOption) {
             if (document.querySelector("#hanseatic").checked) {
-                var name = localStorage.getItem('userName');
-                document.getElementById('congratulationMessage').innerText = 'Congratulations, ' + name + '!';
                 window.location.href="final.html";
             }else {
                 document.getElementById("errorMessage").innerText="Your answer is wrong. Try Again.";}
         }else { 
             document.getElementById("errorMessage").innerText="Choose your answer.";
-
-
         }
     }
 
-
     document.addEventListener("DOMContentLoaded", ()=>{
         document.querySelector("#next").addEventListener("click", goToQuestionThree);
+        $("#prevThree").click(() => location.replace("location.html"));
         
     });
+
+    $(document).ready(()=> {
+        var name=localStorage.getItem('userName'); 
+        document.getElementById('congratulationMessage').innerText = 'Congratulation, ' + name + '!'; 
+        let startTime = new Date(localStorage.getItem('currentTime'));
+        let endTime = new Date();
+        let timeSpent = Math.floor((endTime - startTime) / 1000);
+        document.getElementById('timeSpentOne').innerText = 'Time Spent:' + timeSpent + ' seconds';
+        localStorage.removeItem('currentTime');
+        localStorage.removeItem('userName');
+    });
+
+   
+
+
   
 
 
